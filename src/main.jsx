9779c8581841c001e11f4e705b7395872b8da8de
Karim Dahmani <karim.dahmani88@gmail.com>
2024-02-29 18:13:00 +0100

    test env prod to github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './main.scss'
- import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
- import Header from './components/Header';
- import Footer from './components/Footer';
- import Home from './pages/Home';
- import About from './pages/About';
- import Error from './pages/Error';
- import Details from './pages/Details';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Error from './pages/Error';
+import Details from './pages/Details';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
