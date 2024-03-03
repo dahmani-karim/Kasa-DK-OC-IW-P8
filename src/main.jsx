804c8955ef85bdf467be2eec9649b0f957e2b462
Karim Dahmani <karim.dahmani88@gmail.com>
2024-03-03 16:29:54 +0100

    Update main.jsx and main.scss files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './main.scss'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './main.scss';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
