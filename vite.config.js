991abd1d592b99961d76cbc116913fe44eebaf84
Karim Dahmani <karim.dahmani88@gmail.com>
2024-03-06 21:00:46 +0100

    Update package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import process from 'process'
+//import process from 'process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "Kasa-DK-OC-IW-P8" : "/", // This is the subpath of your project, e.g. /Kasa-DK-OC-IW-P8/,
-  server: {historyApiFallback: true}// This is for the react-router-dom to work properly
+  // base: process.env.NODE_ENV === "production" ? "Kasa-DK-OC-IW-P8" : "/", // This is the subpath of your project, e.g. /Kasa-DK-OC-IW-P8/,
+  // server: {historyApiFallback: true}// This is for the react-router-dom to work properly
 })
