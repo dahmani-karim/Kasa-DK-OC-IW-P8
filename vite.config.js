84d06e0360472c5ec375f0fedf63642e2bd6f8fd
Karim Dahmani <karim.dahmani88@gmail.com>
2024-03-07 11:36:44 +0100

    Update vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-//import process from 'process'
+import process from 'process'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  //  base: '/Kasa-DK-OC-IW-P8/'
-  // server: {historyApiFallback: true}// This is for the react-router-dom to work properly
+  base: process.env.NODE_ENV === "production" ? '/Kasa-DK-OC-IW-P8/' : "/", // This is for the deployment on GitHub Pages
+  server: {historyApiFallback: true} // This is for the react-router-dom to work properly
 })
