4db380a5ff6c82c588fbb4183041357fac41cb88
Karim Dahmani <karim.dahmani88@gmail.com>
2024-03-09 15:39:15 +0100

    Update Router baseline in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,11 @@ import About from './pages/About';
 import Error from './pages/Error';
 import Details from './pages/Details';
 
-const basename = import.meta.env.MODE === "production" ? "/Kasa-DK-OC-IW-P8" : "";
+//const basename = import.meta.env.MODE === "production" ? "/Kasa-DK-OC-IW-P8" : "";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Router basename={basename}>
+      <Router baseline={"/"}>
         <Header />
         <Routes>
             <Route path='/' element={<Home />} />
